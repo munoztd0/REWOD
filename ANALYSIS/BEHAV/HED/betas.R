3b3ba796882bfb3b6a02f859472aff517c7037b4
munoztd0 <david.munoz@etu.unige.com>
2019-09-17 15:25:55 +0200

    betas Hed

--- ANALYSIS/BEHAV/HED/betas.R
+++ ANALYSIS/BEHAV/HED/betas.R
@@ -4,7 +4,7 @@
 
 # -----------------------  PRELIMINARY STUFF ----------------------------------------
 # load libraries
-pacman::p_load(ggplot2, dplyr, plyr, tidyr, reshape, reshape2, Hmisc, corrplot)
+pacman::p_load(ggplot2, dplyr, plyr, tidyr, reshape, reshape2, Hmisc, corrplot, ggpubr)
 
 if(!require(pacman)) {
   install.packages("pacman")
@@ -20,7 +20,10 @@ con2 = 'reward-neutral'
 mod1 = 'lik'
 mod2 = 'int'
 
-# Set working directory
+
+# Set working directory -------------------------------------------------
+
+
 analysis_path <- file.path('~/REWOD/DERIVATIVES/ANALYSIS', task) 
 setwd(analysis_path)
 
@@ -48,6 +51,9 @@ R_N_df$ID <- factor(R_N_df$ID)
 
 
 
+# PLOT FUNCTIONS --------------------------------------------------------------------
+
+
 # plot perceived_likings by time with regression lign
 ggplotRegression <- function (fit) {
   
@@ -55,76 +61,127 @@ ggplotRegression <- function (fit) {
     geom_point() +
     stat_smooth(method = "lm", col = "red") +
     labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
-                       "Intercept =",signif(fit$coef[[1]],5 ),
-                       " Slope =",signif(fit$coef[[2]], 5),
-                       " P =",signif(summary(fit)$coef[2,4], 5)))
+                       #"Intercept =",signif(fit$coef[[1]],5 ),
+                       #" Slope =",signif(fit$coef[[2]], 5),
+                       "  &  P =",signif(summary(fit)$coef[2,4], 5))) +
+    theme(plot.title = element_text(size = 10))
+           
 }
-# Plot R_C liking
+
+
+# PLOT FOR R_C ------------------------------------------------------------
+
+# For liking
 
 lik = R_C_df$lik
-ggplotRegression(lm(R_C_df$AMY_Left~lik))
-ggplotRegression(lm(R_C_df$AMY_Right~lik))
-ggplotRegression(lm(R_C_df$Piri_Left~lik))
-ggplotRegression(lm(R_C_df$Piri_Right~lik))
-ggplotRegression(lm(R_C_df$subcal_Left~lik))
-ggplotRegression(lm(R_C_df$subcal_Right~lik))
+A <- ggplotRegression(lm(R_C_df$AMY_Left~lik)) + rremove("x.title")
+B <- ggplotRegression(lm(R_C_df$AMY_Right~lik)) + rremove("x.title")
+C <- ggplotRegression(lm(R_C_df$Piri_Left~lik)) + rremove("x.title")
+D <- ggplotRegression(lm(R_C_df$Piri_Right~lik)) + rremove("x.title")
+E <- ggplotRegression(lm(R_C_df$subcal_Left~lik)) + rremove("x.title")
+G <- ggplotRegression(lm(R_C_df$subcal_Right~lik))+ rremove("x.title")
 
+figure1 <- ggarrange(A,B,C,D,E,G, 
+                     labels = c("A", "B", "C", "D", "E", "F"),
+                     ncol = 2, nrow = 3) 
 
-# Plot R_C intensity
+annotate_figure(figure1,
+                top = text_grob("Coeficient of determination: REWARD-CONTROL for LIKING", color = "black", face = "bold", size = 14),
+                bottom = "Figure 1", fig.lab.face = "bold")
+# For intensity
 
 int = R_C_df$int
-ggplotRegression(lm(R_C_df$AMY_Left~int))
-ggplotRegression(lm(R_C_df$AMY_Right~int))
-ggplotRegression(lm(R_C_df$Piri_Left~int))
-ggplotRegression(lm(R_C_df$Piri_Right~int))
-ggplotRegression(lm(R_C_df$subcal_Left~int))
-ggplotRegression(lm(R_C_df$subcal_Right~int))
+H <- ggplotRegression(lm(R_C_df$AMY_Left~int)) + rremove("x.title")
+I <- ggplotRegression(lm(R_C_df$AMY_Right~int)) + rremove("x.title")
+J <- ggplotRegression(lm(R_C_df$Piri_Left~int)) + rremove("x.title")
+K <- ggplotRegression(lm(R_C_df$Piri_Right~int)) + rremove("x.title")
+L <- ggplotRegression(lm(R_C_df$subcal_Left~int)) + rremove("x.title")
+M <- ggplotRegression(lm(R_C_df$subcal_Right~int)) + rremove("x.title")
 
-# Plot # Plot R_N liking
+
+figure2 <- ggarrange(H,I,J,K,L,M, 
+                     labels = c("G", "H", "I", "J", "K", "L"),
+                     ncol = 2, nrow = 3) 
+
+annotate_figure(figure2,
+                top = text_grob("Coeficient of determination: REWARD-CONTROL for INTENSITY", color = "black", face = "bold", size = 14),
+                bottom = "Figure 1", fig.lab.face = "bold")
+
+
+
+
+#  Plot for R_N  ----------------------------------------------------------
+
+# For liking
 
 lik = R_N_df$lik
-ggplotRegression(lm(R_N_df$Nacc_Left~lik))
-ggplotRegression(lm(R_N_df$Nacc_Right~lik))
-ggplotRegression(lm(R_N_df$subcal_Left~lik))
-ggplotRegression(lm(R_N_df$subcal_Right~lik))
+N <- ggplotRegression(lm(R_N_df$Nacc_Left~lik)) + rremove("x.title")
+O <- ggplotRegression(lm(R_N_df$Nacc_Right~lik)) + rremove("x.title")
+P <- ggplotRegression(lm(R_N_df$subcal_Left~lik)) + rremove("x.title")
+Q <- ggplotRegression(lm(R_N_df$subcal_Right~lik)) + rremove("x.title")
 
 
-# Plot R_N intensity
+figure3 <- ggarrange(N,O,P,Q, 
+                     labels = c("M", "N", "O", "P"),
+                     ncol = 2, nrow = 3) 
+
+annotate_figure(figure3,
+                top = text_grob("Coeficient of determination: REWARD-NEUTRAL for LIKING", color = "black", face = "bold", size = 14),
+                bottom = "Figure 1", fig.lab.face = "bold")
+
+# For intensity
 
 int = R_N_df$int
-ggplotRegression(lm(R_N_df$Nacc_Left~int))
-ggplotRegression(lm(R_N_df$Nacc_Right~int))
-ggplotRegression(lm(R_N_df$subcal_Left~int))
-ggplotRegression(lm(R_N_df$subcal_Right~int))
+R <- ggplotRegression(lm(R_N_df$Nacc_Left~int)) + rremove("x.title")
+S <- ggplotRegression(lm(R_N_df$Nacc_Right~int)) + rremove("x.title")
+U <- ggplotRegression(lm(R_N_df$subcal_Left~int)) + rremove("x.title")
+V <-  ggplotRegression(lm(R_N_df$subcal_Right~int)) + rremove("x.title")
+
+figure4 <- ggarrange(R,S,U,V, 
+                     labels = c("Q", "R", "S", "T"),
+                     ncol = 2, nrow = 3) 
+
+annotate_figure(figure4,
+                top = text_grob("Coeficient of determination: REWARD-NEUTRAL for INTENSITY", color = "black", face = "bold", size = 14),
+                bottom = "Figure 1", fig.lab.face = "bold")
 
 
 
+# CORRELATIONS R_C------------------------------------------------------------
 
-# Corr R_C
 corr_R_C.rcorr = rcorr(as.matrix(R_C_df))
 corr_R_C.coeff = corr_R_C.rcorr$r[2:7,8:9]
 corr_R_C.p = corr_R_C.rcorr$P[2:7,8:9]
 
-
+# PLOT CORR
 corrplot(corr_R_C.coeff , method = "circle",
          tl.col = "black", tl.srt = 45)
 
-# COrr R_N
+
+# CORRELATIONS R_N ------------------------------------------------------
+
+
 corr_R_N.rcorr = rcorr(as.matrix(R_N_df))
 corr_R_N.coeff = corr_R_N.rcorr$r[2:5,6:7]
 corr_R_N.p = corr_R_N.rcorr$P[2:5,6:7]
+
+# PLOT CORR
 
 corrplot(corr_R_N.coeff , method = "circle",
          tl.col = "black", tl.srt = 45)
 
 
 
-## GET for R_C
-# Get the R^2 and adj R* FOR LIKING
+
+#  Get R.adj & R.squared for R_C ------------------------------------------
+
+
+# FOR LIKING
 R_C_R_squared_lik <- data_frame()
 R_C_R_adj_lik <- data_frame()
 namesRC = c("AMY_Left","AMY_Right", "Piri_Left", "Piri_Right", "subcal_Left", "subcal_Right")
 
+#
 R_C_R_squared_lik[1,1] <- summary(lm(R_C_df$AMY_Left~lik))$r.squared
 R_C_R_adj_lik[1,1] <- summary(lm(R_C_df$AMY_Left~lik))$adj.r.squared
 
@@ -146,11 +203,12 @@ R_C_R_adj_lik[6,1] <- summary(lm(R_C_df$subcal_Right~lik))$adj.r.squared
 R_C_R_adj_lik[,2] <- namesRC 
 R_C_R_squared_lik[,2] <- namesRC 
 
-# Get the R^2 and adj R* FOR INTENSITY
+# FOR INTENSITY
 
 R_C_R_squared_int <- data_frame()
 R_C_R_adj_int <- data_frame()
 
+#
 R_C_R_squared_int[1,1] <- summary(lm(R_C_df$AMY_Left~int))$r.squared
 R_C_R_adj_int[1,1] <- summary(lm(R_C_df$AMY_Left~int))$adj.r.squared
 
@@ -163,21 +221,27 @@ R_C_R_adj_int[3,1] <- summary(lm(R_C_df$Piri_Left~int))$adj.r.squared
 R_C_R_squared_int[4,1] <- summary(lm(R_C_df$Piri_Right~int))$r.squared
 R_C_R_adj_int[4,1] <- summary(lm(R_C_df$Piri_Right~int))$adj.r.squared
 
-R_C_R_squared_int[5,1].SuLe <- summary(lm(R_C_df$subcal_Left~int))$r.squared
+R_C_R_squared_int[5,1] <- summary(lm(R_C_df$subcal_Left~int))$r.squared
 R_C_R_adj_int[5,1] <- summary(lm(R_C_df$subcal_Left~int))$adj.r.squared
 
-R_C_R_squared_int[6,1].SuRi <- summary(lm(R_C_df$subcal_Right~int))$r.squared
+R_C_R_squared_int[6,1] <- summary(lm(R_C_df$subcal_Right~int))$r.squared
 R_C_R_adj_int[6,1] <- summary(lm(R_C_df$subcal_Right~int))$adj.r.squared
 
 R_C_R_adj_int[,2] <- namesRC 
 R_C_R_squared_int[,2] <- namesRC 
 
-# GET for R_N
-# Get the R^2 and adj R* FOR LIKING
+View(R_C_R_adj_int)
+View(R_C_R_squared_int)
+
+
+#  Get R.adj & R.squared for R_N ------------------------------------------
+
+# FOR LIKING
 R_N_R_squared_lik <- data_frame()
 R_N_R_adj_lik <- data_frame()
 namesRN = c("Nacc_Left","Nacc_Right", "subcal_Left", "subcal_Right")
 
+#
 R_N_R_squared_lik[1,1] <- summary(lm(R_N_df$Nacc_Left~lik))$r.squared
 R_N_R_adj_lik[1,1] <- summary(lm(R_N_df$Nacc_Left~lik))$adj.r.squared
 
@@ -193,10 +257,12 @@ R_N_R_adj_lik[4,1] <- summary(lm(R_N_df$subcal_Right~lik))$adj.r.squared
 R_N_R_adj_lik[,2] <- namesRN 
 R_N_R_squared_lik[,2] <- namesRN 
 
-# Get the R^2 and adj R* FOR INTENSITY
+
+# FOR INTENSITY
 R_N_R_squared_int <- data_frame()
 R_N_R_adj_int<- data_frame()
 
+#
 R_N_R_squared_int[1,1] <- summary(lm(R_N_df$Nacc_Left~int))$r.squared
 R_N_R_adj_int[1,1] <- summary(lm(R_N_df$Nacc_Left~int))$adj.r.squared
 
@@ -211,6 +277,9 @@ R_N_R_adj_int[4,1] <- summary(lm(R_N_df$subcal_Right~int))$adj.r.squared
 
 R_N_R_adj_int[,2] <- namesRN 
 R_N_R_squared_int[,2] <- namesRN 
+
+View(R_N_R_adj_int)
+View(R_N_R_squared_int)
 
 ## SUMMARY ##
 # so basically contrast 
